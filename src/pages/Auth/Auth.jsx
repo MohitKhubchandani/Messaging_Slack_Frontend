a9b4cbec16c1450e483221cd8286d03e4d75075c
Mohit04 <mohitkhubchandani88@gmail.com>
2025-06-28 03:16:56 +0530

    Added a custom color

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -6,7 +6,7 @@ import { SignupCard } from '@/components/organisms/Auth/SignupCard';
 export const Auth = () => {
     return (
         <>
-        <div className="h-[100vh] flex justify-center items-center bg-[#5c3858]">
+        <div className="h-[100vh] flex justify-center items-center bg-slack">
             <div className="md:h-auto md:w-[420px]">
                 <SignupCard/>                
             </div>
